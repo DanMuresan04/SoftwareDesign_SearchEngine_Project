2cc04f0933446e503d771bd6b9a089281ef8e6e8
DanMuresan04 <muresandanut0401@gmail.com>
2026-03-30 06:46:12 +0300

    Update FileHandler.java

--- handler/FileHandler.java
+++ handler/FileHandler.java
@@ -1,6 +1,7 @@
 package handler;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public interface FileHandler {
-    void handle(Path path);
+    void handle(Path filePath, BasicFileAttributes attrs);
 }
